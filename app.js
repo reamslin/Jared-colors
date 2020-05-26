c15cc04372ea3b36eaff4351c3b99164577cc683
reamslin <reamslin@gmail.com>
2020-05-26 09:34:15 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+
+function randomRGB() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    // what should we return?
+}
+
+// get h1 from document
+const word = 
+
+setInterval(function() {
+    // What to do each time
+
+},// how often? (in ms)
+ );
